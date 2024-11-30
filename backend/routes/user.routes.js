3419a9a4d3e8eaf6d4e70e366ea55e86a302358a
Thrishavigi <thrishavijayakumar7@gmail.com>
2024-11-30 21:25:29 +0530

    user-routes

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const router = express.Router();
+import { getUserProfile, followUnfollowUser,getSuggestedUsers,updateUser} from '../controllers/user.controller.js';
+import { protectRoute} from '../middleware/protectRoute.js';
+
+router.get("/profile/:username",protectRoute,getUserProfile);
+router.get("/suggested",protectRoute,getSuggestedUsers);
+router.get("/follow/:id",protectRoute,followUnfollowUser);
+router.get("/update",protectRoute,updateUser);
+export default router;
