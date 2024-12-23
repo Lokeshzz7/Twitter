322db7dba0c8b0a3263b23353f30ee1808073a87
Thrishavigi <thrishavijayakumar7.com>
2024-12-23 12:53:24 +0530

    ok

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      `MONGO_URI=mongodb+srv://manithri7:JjdbYchwQeSF8VJA@cluster0.t2djb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connection to mongoDB: ${error.message}`);
