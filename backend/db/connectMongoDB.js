702b6ccf51104608240925336ff30a2c050f7d14
Lokeshzz7 <waranlokesh688@gmail.com>
2024-12-23 22:25:14 +0530

    first

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      `mongodb+srv://manithri7:JjdbYchwQeSF8VJA@cluster0.t2djb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-    );
+    const conn = await mongoose.connect(process.env.MONGODB);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connection to mongoDB: ${error.message}`);
